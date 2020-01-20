f4eb0063a409b12d45172800db3239cc2aef03bc
TimothyTedaldi <TimothyTedaldi@users.noreply.github.com>
2020-01-20 13:45:28 +0100

    still working on 9.1

--- 1.exercises/09-misc/01-waving-text/script.js
+++ 1.exercises/09-misc/01-waving-text/script.js
@@ -11,4 +11,35 @@
 
 (() => {
     // your code here
+    document.getElementById('target').textContent=''
+    
+    
+    let aBeautifulEpiphany = "Rose are red Violets are blue Sugar is sweet And so is my cat!";
+    let stringArray = (aBeautifulEpiphany.split(' '));
+    
+    let cptTotalLetters = 0;
+    for (let x=0; x<stringArray.length; x++){
+        cptTotalLetters += (stringArray[x].length);
+    }
+    
+    document.getElementById('target').setAttribute('style', 'white-space: pre;');
+    
+    let cpt=0;let isItUpper = false; let anotherCpt = 0;
+    for(let x=0; x<stringArray.length; x++){
+        for(let y=0; y<stringArray[cpt].length; y++){
+            if(stringArray[x][y] == stringArray[x][y].toUpperCase()  && x!=0){
+                document.getElementById('target').textContent += '\r\n'
+            }
+            if(((anotherCpt+1) % 5) == 0 || (anotherCpt+1) == 1 ){
+                document.getElementById('target').style.fontSize = "12px";
+            }
+            else{
+                document.getElementById('target').style.fontSize = "50px";
+            }
+            document.getElementById('target').textContent += stringArray[x][y];
+            anotherCpt++;
+        }
+        document.getElementById('target').textContent += ' ';
+        cpt++;
+    }
 })();
